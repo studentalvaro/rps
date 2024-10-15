72e3377e3e34165ca0468431cdcbb362c382bd2f
Álvaro Beltrán Santiago <138005473+studentalvaro@users.noreply.github.com>
2024-10-15 15:55:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Alvaro Beltran - Broken Rock Paper Scissors 1d1ba0c1</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
+
